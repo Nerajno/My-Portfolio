4ed339f4f3d7c75219fdc4e519e24621f20c3113
nerajno <nerajno@gmail.com>
2021-03-05 18:43:42 -0500

    making simple additions

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const message = " Web developer with a background in STEM education"
+const message = " A Web Developer with a background in STEM education"
 
 let index=0;
 function writeMessage(){
     document.querySelector(".text-message").innerText= message.slice(0, index);
     index++;
-
-    
 }
 
 setInterval(writeMessage, 200)
